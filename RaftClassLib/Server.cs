3595ed5348ad3ca73e116633ff508446cfb6cd65
aaronforsnow <twodoschool@gmail.com>
2025-01-14 21:28:32 -0700

    Added Test 1

--- RaftClassLib/Server.cs
+++ RaftClassLib/Server.cs
@@ -3,8 +3,30 @@
 public class Server
 {
     public ServerState State;
+    public List<string> Sentmessages = new List<string>();
+    public bool IsLive;
+    public Server()
+    {
+        State = ServerState.Follower;
+        IsLive = true;
+        CheckMessages();
+    }
+    private void CheckMessages()
+    {
+        while (IsLive)
+        {
+            Thread.Sleep(100);
+            Sentmessages.Add("HB");
+            if (Sentmessages.Count > 2)
+            {
+                IsLive = false;
+            }
+        }
+    }
 }
 public enum ServerState
 {
-    Follower
+    Follower,
+    Candadate,
+    Leader
 }
